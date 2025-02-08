9981999b2af0ac73d967517cdb987cb9355aef4b
JasonJiaxiangLi <jasonljx96@gmail.com>
2025-02-08 13:51:13 -0600

    change wandb entity

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ source activate /opt/conda/envs/handbook
 accelerate launch --config_file ./configs/zero3.yaml \
     --num_processes=8 \
     grpo_demo.py \
+    --wandb_entity="mhong-university-of-minnesota" \
     --model_name="meta-llama/Llama-3.2-1B-Instruct"
 
 # --model_name="Qwen/Qwen2.5-1.5B-Instruct"
